a1afe8ece9dc2f9a0c1e29e0f8bcea50391b3837
Andrei <sucmanandrei@gmail.com>
2025-02-04 09:04:28 +0300

    bug fixing

--- deleteAccounts/dist/init.js
+++
@@ -1,7 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import { MongoClient } from "mongodb";
-const client = new MongoClient(process.env.DATABASE_URI);
-const db = client.db(process.env.DATABASE_NAME);
-const adminDb = client.db(process.env.ADMIN_DATABASE_NAME);
-export { client, db, adminDb };
